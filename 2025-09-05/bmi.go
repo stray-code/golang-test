bbe59842ef9f264d3f5ba5cc28fa1cdc11b5cfba
stray-code <abc1007020@gmail.com>
2025-09-05 00:12:31 +0900

    CalculateBMIメソッドを定義

--- 2025-09-05/bmi.go
+++ 2025-09-05/bmi.go
@@ -8,6 +8,10 @@ type User struct {
 	Weight float64
 }
 
+func (u *User) CalculateBMI() float64 {
+	return u.Weight / (u.Height * u.Height)
+}
+
 func main() {
 	user := User{
 		Name:   "George",
@@ -15,7 +19,7 @@ func main() {
 		Weight: 65.3,
 	}
 
-	bmi := user.Weight / (user.Height * user.Height)
+	bmi := user.CalculateBMI()
 
 	fmt.Printf("%sのBMIは%.2fです。\n", user.Name, bmi)
 }
