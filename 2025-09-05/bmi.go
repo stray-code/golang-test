928744abfc25016969c740d4ead0ed5f6ae85ab5
stray-code <abc1007020@gmail.com>
2025-09-05 00:28:17 +0900

    身長のバリデーションを追加

--- 2025-09-05/bmi.go
+++ 2025-09-05/bmi.go
@@ -8,18 +8,27 @@ type User struct {
 	Weight float64
 }
 
-func (u *User) CalculateBMI() float64 {
-	return u.Weight / (u.Height * u.Height)
+func (u *User) CalculateBMI() (float64, error) {
+	if u.Height <= 0 {
+		return 0, fmt.Errorf("身長は0より大きい値である必要があります")
+	}
+
+	return u.Weight / (u.Height * u.Height), nil
 }
 
 func main() {
 	user := User{
-		Name:   "George",
-		Height: 1.75,
+		Name: "George",
+		// Height: 1.75,
+		Height: 0,
 		Weight: 65.3,
 	}
 
-	bmi := user.CalculateBMI()
+	bmi, error := user.CalculateBMI()
+	if error != nil {
+		fmt.Printf("BMTの計算に失敗しました：%v\n", error)
+	} else {
+		fmt.Printf("%sのBMIは%.2fです。\n", user.Name, bmi)
+	}
 
-	fmt.Printf("%sのBMIは%.2fです。\n", user.Name, bmi)
 }
