57d1866b0a76505ae8dfa30812d3acc34de0278f
stray-code <abc1007020@gmail.com>
2025-09-07 00:14:30 +0900

    値の出力を作成

---
+++ 2025-09-07/02_simple_values.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("%d\n", 1+3)
+
+	fmt.Println("hello golang")
+
+	fmt.Println(true)
+	fmt.Println(false)
+
+	fmt.Println(3.11)
+	fmt.Println(7.0 / 3.0)
+}
