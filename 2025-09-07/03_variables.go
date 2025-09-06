c9440756eb223d8c594bc04288b6179a3795c4c8
stray-code <abc1007020@gmail.com>
2025-09-07 07:19:19 +0900

    変数宣言の勉強

---
+++ 2025-09-07/03_variables.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "golang"
+	fmt.Println(name)
+
+	var isAdult = true
+	fmt.Println(isAdult)
+
+	age := 42
+	fmt.Println(age)
+
+	var greet string
+	greet = "good morning"
+	fmt.Println(greet)
+
+	price := 23.5
+	fmt.Println(price)
+}
