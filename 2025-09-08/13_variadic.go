ae225b91a720428891e62d8a46e130affe88eda2
stray-code <abc1007020@gmail.com>
2025-09-08 21:00:48 +0900

    variadicを勉強

---
+++ 2025-09-08/13_variadic.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func sum(nums ...int) int {
+	total := 0
+
+	for _, num := range nums {
+		total += num
+	}
+
+	return total
+}
+
+func main() {
+	nums := []int{1, 2, 3, 4, 5}
+
+	result := sum(nums...)
+
+	fmt.Println(result)
+}
