0d0ff4b8551b2335049ba1ac5d4df47fc9adf206
stray-code <abc1007020@gmail.com>
2025-09-17 21:58:15 +0900

    配列とスライスを勉強

---
+++ 2025-09-17/arrays-slices.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a [2]string
+	a[0] = "Hello"
+	a[1] = "World"
+	fmt.Println(a[0], a[1])
+	fmt.Println(a)
+
+	primes := [6]int{2, 3, 5, 7, 11, 13}
+	fmt.Println(primes)
+
+	var s = primes[3:5]
+	fmt.Println(s)
+
+	names := [4]string{
+		"John",
+		"Paul",
+		"George",
+		"Ringo",
+	}
+	fmt.Println(names)
+
+	b := names[0:2]
+	c := names[1:3]
+	fmt.Println(b, c)
+	fmt.Println(names)
+}
